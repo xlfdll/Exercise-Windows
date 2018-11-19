7aecc0b9a4df8470cc89ff4b6045dfb4abcae7ec
xlfdll <xlfdll@gmail.com>
2018-11-18 21:35:44 -0800

    Added Hello Windows program file for Windows API practice

---
+++ Windows API/HelloWindows.c
@@ -0,0 +1,87 @@
+#include <Windows.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
+{
+	static TCHAR szAppName[] = TEXT("HelloWindows");
+	static TCHAR szAppTitle[] = TEXT("Hello Windows!");
+	HWND hwnd;
+	MSG msg;
+	WNDCLASS wndclass;
+
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(NULL, IDI_WINLOGO);
+	wndclass.hCursor = LoadCursor(NULL, IDC_CROSS);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wndclass.lpszMenuName = NULL;
+	wndclass.lpszClassName = szAppName;
+
+	if (!RegisterClass(&wndclass))
+	{
+		MessageBox(NULL, TEXT("This program requires Windows NT!"), szAppTitle, MB_ICONERROR);
+
+		return 0;
+	}
+
+	hwnd = CreateWindow(
+		szAppName,
+		szAppTitle,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	ShowWindow(hwnd, iCmdShow);
+	UpdateWindow(hwnd);
+
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc;
+	PAINTSTRUCT ps;
+	RECT rect;
+
+	switch (message)
+	{
+	case WM_CREATE:
+		// Need to include WINMM.LIB library in linker's properties
+		PlaySound(TEXT("C:\\Windows\\Media\\Alarm05.wav"), NULL, SND_FILENAME | SND_ASYNC);
+		return 0;
+	case WM_PAINT:
+		hdc = BeginPaint(hwnd, &ps);
+
+		GetClientRect(hwnd, &rect);
+
+		DrawText(hdc, TEXT("Hello, Windows NT!"), -1, &rect, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+
+		EndPaint(hwnd, &ps);
+
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+
+		return 0;
+	default:
+		break;
+	}
+
+	return DefWindowProc(hwnd, message, wParam, lParam);
+}
